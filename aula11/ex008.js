c2c61e2e18195e993ab5265223f776c4e207548a
Faylon Camargo <92471708+fayloncam@users.noreply.github.com>
2021-10-23 09:30:16 -0300

    Aula 11
    Progresso dos exercícios 8, 9, 10 e 11 da aula 11 do Curso em Video de  JavaScript

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 70
+console.log(`A velocidade do seu carro é ${velocidade} Km/h`)
+
+if (velocidade > 60) {
+    console.log("Você ultrapassou a velocidade máxima! MULTADO")
+}
+console.log('Dirija com cuidado')
